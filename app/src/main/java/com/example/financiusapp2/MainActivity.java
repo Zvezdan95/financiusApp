f64f9928b4a5e3dd8c58230afccc8b1bac1146f3
Zvezdan95 <zvezdann11@gmail.com>
2021-08-29 14:26:33 +0200

    full app

---
+++ app/src/main/java/com/example/financiusapp2/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.financiusapp2;
+import android.graphics.Bitmap;
+
+import android.os.Build;
+import android.os.Bundle;
+
+//import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.webkit.PermissionRequest;
+
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.ArrayList;
+import java.util.List;
+public class MainActivity extends AppCompatActivity implements MyWebChromeClient.ProgressListener{
+    private ProgressBar chromeProgressBar;
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        myWebView = (WebView) findViewById(R.id.webView);
+        //chromeProgressBar = (ProgressBar) findViewById(R.id.progressBarChrome);
+        //Settings
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setUserAgentString("coming from Financius app");
+        myWebView.getSettings().setTextSize(WebSettings.TextSize.NORMAL);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            webSettings.setAllowUniversalAccessFromFileURLs(true);
+            webSettings.setAllowFileAccessFromFileURLs(true);
+        }
+
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setAllowFileAccess(true);
+
+        //webSettings.setLoadsImagesAutomatically(true);
+        //inizialize client
+
+        //load website by URL
+
+        myWebView.loadUrl("https://39c7-212-200-204-128.ngrok.io");
+        //register token for notification
+
+        // this.onStart();
+        chromeProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            // Grant permissions for cam
+
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    request.grant(request.getResources());
+                }
+            }
+        });
+
+        myWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                chromeProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                chromeProgressBar.setVisibility(View.GONE);
+
+            }
+        });
+    }
+
+    @Override
+    public void onUpdateProgress(int progressValue) {
+        chromeProgressBar.setProgress(progressValue);
+        if (progressValue == 100) {
+            chromeProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public class WebViewController extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(myWebView.canGoBack())
+            myWebView.goBack();
+        else
+            super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/example/financiusapp2/QRcodeScanner.java
@@ -0,0 +1,4 @@
+package com.example.financiusapp2;
+
+public class QRcodeScanner {
+}
